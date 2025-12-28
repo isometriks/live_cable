36b9c6c3cbdd08bdcf8cd35ad99e0c84209535cc
Craig Blanchette <craig.blanchette@gmail.com>
2025-12-28 13:41:06 -0500

    Split up connection into modules as well

---
+++ lib/live_cable/connection/state_management.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module LiveCable
+  class Connection
+    module StateManagement
+      extend ActiveSupport::Concern
+
+      def get(container_name, component, variable, initial_value)
+        containers[container_name] ||= Container.new
+        containers[container_name][variable] ||= process_initial_value(component, variable, initial_value)
+      end
+
+      def set(container_name, variable, value)
+        dirty(container_name, variable)
+
+        containers[container_name] ||= Container.new
+        containers[container_name][variable] = value
+      end
+
+      def dirty(container_name, *variables)
+        containers[container_name] ||= Container.new
+        containers[container_name].mark_dirty(*variables)
+      end
+
+      def reset_changeset
+        containers.each_value(&:reset_changeset)
+      end
+
+      private
+
+      def process_initial_value(component, variable, initial_value)
+        case initial_value
+        when nil
+          nil
+        when Proc
+          args = []
+          args << component if initial_value.arity.positive?
+
+          initial_value.call(*args)
+        else
+          raise Error, "Initial value for \":#{variable}\" must be a proc or nil"
+        end
+      rescue StandardError => e
+        handle_error(component, e)
+      end
+    end
+  end
+end
