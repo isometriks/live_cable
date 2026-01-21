2659be4ff54d4fe1e64b79c125ebe7c4a4001bb1
Craig Blanchette <craig.blanchette@gmail.com>
2026-01-21 12:25:44 -0500

    Prevent re-rendering children if parent already rendered them

--- lib/live_cable/connection/broadcasting.rb
+++ lib/live_cable/connection/broadcasting.rb
@@ -6,15 +6,20 @@ module LiveCable
       extend ActiveSupport::Concern
 
       def broadcast_changeset
+        rendered = []
         shared_changeset = containers[SHARED_CONTAINER]&.changeset
 
         # Use a copy of the components since new ones can get added while rendering
         # and causes an issue here.
         components.values.dup.each do |component|
+          # Component may have already been re-rendered by a parent, so don't render it again
+          next if rendered.include?(component)
+
           container = containers[component.live_id]
 
           if container&.changed? || component.shared_reactive_variables.intersect?(shared_changeset)
             component.broadcast_render
+            rendered |= component.rendered_children
           end
         end
       end
