15eb69aec04560362888339329be692ced4f92e8
Craig Blanchette <craig.blanchette@gmail.com>
2026-03-18 21:37:49 -0400

    Handle component errors in more places

--- spec/dummy/app/live/error_test.rb
+++ spec/dummy/app/live/error_test.rb
@@ -2,10 +2,26 @@
 
 module Live
   class ErrorTest < LiveCable::Component
-    actions :trigger_error
+    STREAM_NAME = 'live_cable_error_stream_test'
+
+    reactive :raise_on_render, -> { false }
+
+    actions :trigger_error, :trigger_render_error, :subscribe_to_error_stream, :broadcast_to_error_stream
 
     def trigger_error
       raise 'Something went wrong'
+    end
+
+    def trigger_render_error
+      self.raise_on_render = true
+    end
+
+    def subscribe_to_error_stream
+      stream_from(STREAM_NAME) { raise 'Stream callback error' }
+    end
+
+    def broadcast_to_error_stream
+      ActionCable.server.broadcast(STREAM_NAME, {})
     end
   end
 end
