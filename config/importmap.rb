c265ade9f633a162421cdd3fc4295b92bc2d7e1c
Craig Blanchette <craig.blanchette@gmail.com>
2026-03-25 20:38:04 -0400

    Pin other libraries we need

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+pin '@rails/actioncable', to: 'actioncable.esm.js'
+pin 'morphdom', to: 'https://ga.jspm.io/npm:morphdom@2.7.8/dist/morphdom-esm.js'
+
 pin 'live_cable_controller', to: 'controllers/live_controller.js'
 pin 'live_cable_blessing', to: 'live_cable_blessing.js'
 pin 'live_cable_subscriptions', to: 'subscriptions.js'
